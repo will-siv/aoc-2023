5783c03da5043ce401f74d6469ca357eb8255bb3
will-siv <wsimkinsiv@gmail.com>
2023-12-05 02:25:59 -0500

    day3 - unfinished but i'm tired /shrug

---
+++ day3/solution.py
@@ -0,0 +1,48 @@
+
+def numFromArray(a):
+  num = 0
+  mult = 1
+  while len(a) > 0:
+    num += a.pop(-1) * mult
+    mult *= 10
+
+  return num
+
+grid = []
+with open("data.txt", 'r') as r:
+  for line in r:
+    grid.append(line)
+
+length = len(grid[0])
+height = len(grid)
+
+s = 0
+for i in range(height):
+  numArray = []
+  isPart = False
+  for j in range(length):
+    try:
+      char = int(grid[i][j])
+    except ValueError:
+      if isPart:
+        isPart = False
+        num = numFromArray(numArray)
+        s += num
+      numArray = []
+      continue
+    numArray.append(char)
+    # check surrounded
+    for k in range(9):
+      try:
+        i_off = k//3 - 1 + i
+        j_off = k%3 - 1 + j
+        test = grid[i_off][j_off]
+      except IndexError:
+        continue
+      if i_off < 0 or j_off < 0:
+        continue
+      if not ('.' == test or test.isnumeric()):
+        isPart = True
+        break
+
+print(s)
